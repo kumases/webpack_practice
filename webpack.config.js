22505123ac6c78039f8f569ea9e00184c4d98971
kuma <105776488+kumases@users.noreply.github.com>
2022-05-19 15:59:18 +0900

    Load images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports ={
                     },
                 ],
             },
+            {
+                test: /\.(png|jpg)/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            esModule: false,
+                            name:'images/[name].[ext]',
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
